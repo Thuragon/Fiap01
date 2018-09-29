d692a3e98d31e0c2b1eb0d8a316f9b08d0357eb7
Joao Roberto <joao.rjunior87@gmail.com>
2018-09-29 15:56:59 -0300

    fim da primeira aula

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap01.Data;
+using Fiap01.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,50 @@ namespace Fiap01.Controllers
 {
     public class HomeController : Controller
     {
+        private PerguntasContext _context;
+
+        public HomeController (PerguntasContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
+        {
+            
+
+
+
+            //ViewBag.Nome = "Joao Roberto";
+            //ViewData["MeuNome"] = $"É é meu nome mesmo {DateTime.Now}";
+
+            //var pergunta = new Pergunta()
+            //{
+            //    Id = 1,
+            //    Descricao = "Que hroas é a chamada?",
+            //    Autor = "Zézinho"
+            //};
+
+            return View(_context.Perguntas.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Pergunta pergunta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Perguntas.Add(pergunta);
+                await _context.SaveChangesAsync();
+                var salvarNoBanco = pergunta;
+            }
+            return View();
+        }
+
         public IActionResult Ajuda()
         {
             return View();
---
+++ Models/Pergunta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiap01.Models
+{
+    public class Pergunta
+    {
+        public Pergunta()
+        {
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        public string Autor { get; set; }
+    }
+}
